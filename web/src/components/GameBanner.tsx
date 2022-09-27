d66b2631ad5eefb27d85e55560953eb3d16016a0
Tiago Junio <tiago@Junio.localdomain>
2022-09-27 07:45:29 -0300

    fix:atualização aplicação

---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,24 @@
+
+
+interface GameBannerProps {
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+
+}
+
+
+export function GameBanner(props: GameBannerProps) {
+
+
+  return (
+    <a href="" className="relative rounded-lg overflow-hidden">
+      <img src={props.bannerUrl} alt={props.title} />
+
+      <div className=" bg-game-gradient w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0" >
+        <strong className="font-bold text-white block">{props.title}</strong>
+        <span className="text-zinc-300 text-sm block">{props.adsCount} anúncio(s)</span>
+      </div>
+    </a>
+  );
+} 
